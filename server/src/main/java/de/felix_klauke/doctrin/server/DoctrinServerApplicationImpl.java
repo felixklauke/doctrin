66825ef665230608abeb7cc3eb4558aecd89d2cf
Felix Klauke <info@felix-klauke.de>
2018-01-26 12:26:44 +0100

    Add server application alongside with di module and corresponding tests.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ server/src/main/java/de/felix_klauke/doctrin/server/DoctrinServerApplicationImpl.java
@@ -0,0 +1,12 @@
+package de.felix_klauke.doctrin.server;
+
+/**
+ * @author Felix Klauke <fklauke@itemis.de>
+ */
+public class DoctrinServerApplicationImpl implements DoctrinServerApplication {
+
+    @Override
+    public void initialize() {
+
+    }
+}
---
+++ server/src/test/java/de/felix_klauke/doctrin/server/DoctrinServerApplicationImplTest.java
@@ -0,0 +1,18 @@
+package de.felix_klauke.doctrin.server;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * @author Felix Klauke <fklauke@itemis.de>
+ */
+public class DoctrinServerApplicationImplTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
