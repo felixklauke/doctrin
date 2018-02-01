bbe646dec2b1dfafab848b76aa0709feaa72144f
Felix Klauke <info@felix-klauke.de>
2018-02-01 08:54:50 +0100

    Add dummy test for commons.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ commons/src/test/java/de/felix_klauke/doctrin/commons/DoctrinCommonsTest.java
@@ -0,0 +1,22 @@
+package de.felix_klauke.doctrin.commons;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Felix Klauke <fklauke@itemis.de>
+ */
+public class DoctrinCommonsTest {
+
+    private DoctrinCommons doctrinCommons;
+
+    @Before
+    public void setUp() {
+        doctrinCommons = new DoctrinCommons();
+    }
+
+    @Test
+    public void on() {
+        
+    }
+}
