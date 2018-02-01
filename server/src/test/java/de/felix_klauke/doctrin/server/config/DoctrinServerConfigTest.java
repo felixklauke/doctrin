d4e26fdd5a73482535d1a4d722dca69faf3abfb0
Felix Klauke <info@felix-klauke.de>
2018-02-01 09:12:08 +0100

    Feature/codecov (#12)
    * Add codecov.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add test for server config.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ server/src/test/java/de/felix_klauke/doctrin/server/config/DoctrinServerConfigTest.java
@@ -0,0 +1,32 @@
+package de.felix_klauke.doctrin.server.config;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Felix Klauke <fklauke@itemis.de>
+ */
+public class DoctrinServerConfigTest {
+
+    private static final String TEST_HOST = "localhost";
+    private static final int TEST_PORT = 8089;
+
+    private DoctrinServerConfig doctrinServerConfig;
+
+    @Before
+    public void setUp() {
+        doctrinServerConfig = new DoctrinServerConfig(TEST_HOST, TEST_PORT);
+    }
+
+    @Test
+    public void getPort() {
+        assertEquals(TEST_HOST, doctrinServerConfig.getHost());
+    }
+
+    @Test
+    public void getHost() {
+        assertEquals(TEST_PORT, doctrinServerConfig.getPort());
+    }
+}
