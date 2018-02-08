d573a1f1d572c3235eaf8cddd51fabebb77e849d
Felix Klauke <info@felix-klauke.de>
2018-02-08 14:07:44 +0100

    Test channel handler validation.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- server/src/test/java/de/felix_klauke/doctrin/server/channel/DoctrinServerChannelInitializerTest.java
+++ server/src/test/java/de/felix_klauke/doctrin/server/channel/DoctrinServerChannelInitializerTest.java
@@ -1,13 +1,18 @@
 package de.felix_klauke.doctrin.server.channel;
 
+import de.felix_klauke.doctrin.commons.message.DoctrinMessageWrapper;
 import de.felix_klauke.doctrin.core.DoctrinCoreApplicationImpl;
 import de.felix_klauke.doctrin.server.DoctrinServerApplicationImpl;
 import de.felix_klauke.doctrin.server.connection.DoctrinNettyServerConnection;
+import de.felix_klauke.doctrin.server.connection.DoctrinServerConnection;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.reactivex.Observable;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * @author Felix Klauke <fklauke@itemis.de>
@@ -26,5 +31,35 @@ public class DoctrinServerChannelInitializerTest {
         NioServerSocketChannel channel = new NioServerSocketChannel();
         doctrinServerChannelInitializer.initChannel(channel);
         assertTrue(channel.pipeline().toMap().size() > 0);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void initChannel1() {
+        doctrinServerChannelInitializer = new DoctrinServerChannelInitializer(() -> new DoctrinServerConnection() {
+            @Override
+            public Observable<DoctrinMessageWrapper> getMessages() {
+                return null;
+            }
+
+            @Override
+            public void sendMessage(JSONObject jsonObject) {
+
+            }
+
+            @Override
+            public Observable<String> getRemoteName() {
+                return null;
+            }
+
+            @Override
+            public void setRemoteName(String remoteName) {
+
+            }
+        }, new DoctrinServerApplicationImpl(new DoctrinCoreApplicationImpl()));
+
+        NioServerSocketChannel channel = new NioServerSocketChannel();
+        doctrinServerChannelInitializer.initChannel(channel);
+
+        fail();
     }
 }
