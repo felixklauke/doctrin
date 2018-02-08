d137284818a224863156281c45a613d304e4d359
Felix Klauke <info@felix-klauke.de>
2018-02-08 09:57:39 +0100

    Add remote name feature.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ server/src/test/java/de/felix_klauke/doctrin/server/connection/DoctrinNettyServerConnectionTest.java
@@ -0,0 +1,44 @@
+package de.felix_klauke.doctrin.server.connection;
+
+import io.reactivex.Observable;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Felix Klauke <fklauke@itemis.de>
+ */
+public class DoctrinNettyServerConnectionTest {
+
+    private DoctrinNettyServerConnection doctrinNettyServerConnection;
+
+    @Before
+    public void setUp() {
+        doctrinNettyServerConnection = new DoctrinNettyServerConnection();
+    }
+
+    @Test
+    public void getMessages() {
+        Observable<JSONObject> messages = doctrinNettyServerConnection.getMessages();
+
+        assertNotNull(messages);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void sendMessage() {
+        doctrinNettyServerConnection.sendMessage(new JSONObject());
+    }
+
+    @Test
+    public void setRemoteName() {
+        assertNotNull(doctrinNettyServerConnection.getRemoteName());
+
+        doctrinNettyServerConnection.setRemoteName("test-01");
+
+        assertNotNull(doctrinNettyServerConnection.getRemoteName());
+        assertEquals("test-01", doctrinNettyServerConnection.getRemoteName());
+    }
+}
