08c2bf4c33c53419bed95e1c420b966b40b7950c
Felix Klauke <info@felix-klauke.de>
2018-03-01 22:33:04 +0100

    Add basic client side subscription.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- client/src/main/java/de/felix_klauke/doctrin/client/DoctrinClient.java
+++ client/src/main/java/de/felix_klauke/doctrin/client/DoctrinClient.java
@@ -1,9 +1,18 @@
 package de.felix_klauke.doctrin.client;
 
+import io.reactivex.Observable;
+import org.json.JSONObject;
+
 /**
  * @author Felix Klauke <fklauke@itemis.de>
  */
 public interface DoctrinClient {
 
-
+    /**
+     * Subscribe to channel with the given name.
+     *
+     * @param channelName The channel name.
+     * @return The channel name.
+     */
+    Observable<JSONObject> subscribeChannel(String channelName);
 }
