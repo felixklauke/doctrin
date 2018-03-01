e916dae6be2ddea5341f687342f701a1a6b6c1b4
Felix 'SasukeKawaii' Klauke <info@felix-klauke.de>
2018-03-01 22:36:48 +0100

    Feature/client (#32)
    * Refactor dependencies, add client impl and corresponding test.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add basic client side subscription.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- client/src/main/java/de/felix_klauke/doctrin/client/DoctrinClientImpl.java
+++ client/src/main/java/de/felix_klauke/doctrin/client/DoctrinClientImpl.java
@@ -1,8 +1,33 @@
 package de.felix_klauke.doctrin.client;
 
+import com.google.common.collect.Maps;
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
+import org.json.JSONObject;
+
+import java.util.Map;
+
 /**
  * @author Felix Klauke <fklauke@itemis.de>
  */
 public class DoctrinClientImpl implements DoctrinClient {
 
+    /**
+     * The storage for all active subscriptions. Will be used as a cache for duplication reasons.
+     */
+    private final Map<String, PublishSubject<JSONObject>> subscriptions = Maps.newConcurrentMap();
+
+    @Override
+    public Observable<JSONObject> subscribeChannel(String channelName) {
+        Observable<JSONObject> subscription = subscriptions.get(channelName);
+
+        if (subscription != null) {
+            return subscription;
+        }
+
+        PublishSubject<JSONObject> subject = PublishSubject.create();
+        subscriptions.put(channelName, subject);
+
+        return subject;
+    }
 }
--- client/src/test/java/de/felix_klauke/doctrin/client/DoctrinClientImplTest.java
+++ client/src/test/java/de/felix_klauke/doctrin/client/DoctrinClientImplTest.java
@@ -1,6 +1,12 @@
 package de.felix_klauke.doctrin.client;
 
+import io.reactivex.Observable;
+import org.json.JSONObject;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * @author Felix Klauke <fklauke@itemis.de>
@@ -12,5 +18,20 @@ public class DoctrinClientImplTest {
     @Before
     public void setUp() {
         doctrinClient = new DoctrinClientImpl();
+    }
+
+    @Test
+    public void testSubscribeChannel() {
+        Observable<JSONObject> observable = doctrinClient.subscribeChannel("test");
+
+        assertNotNull(observable);
+    }
+
+    @Test
+    public void testSubscribeChannelWithCacheHit() {
+        Observable<JSONObject> observable = doctrinClient.subscribeChannel("test");
+        Observable<JSONObject> test = doctrinClient.subscribeChannel("test");
+
+        assertEquals(observable, test);
     }
 }
