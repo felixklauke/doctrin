297a29d1dafd4d509ee67d64cc932c29e2ff5673
Felix Klauke <info@felix-klauke.de>
2018-09-07 20:15:00 +0200

    Tabula Rasa.

--- commons/src/main/java/de/felix_klauke/doctrin/commons/message/ActionCode.java
+++
@@ -1,9 +0,0 @@
-package de.felix_klauke.doctrin.commons.message;
-
-/**
- * @author Felix Klauke <fklauke@itemis.de>
- */
-public enum ActionCode {
-
-    UPDATE_SUBSCRIBER_NAME, SUBSCRIBE, UNSUBSCRIBE, PUBLISH, UNKNOWN, PUBLISH_OTHER, BULK_SUBSCRIBE
-}
